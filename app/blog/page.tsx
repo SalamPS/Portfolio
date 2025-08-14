69aa92c1eb31f0e5c40c71fc1984d030720bb27b
SalamPS <salamp@salamp.id>
2025-08-14 19:28:36 +0700

    feat(blog): Blog post list

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,15 @@
 import BlogList from "@/components/blog/BlogList"
+import Jumbotron from "@/components/global/Jumbotron"
 import GlobalNavbar from "@/components/global/Navbar"
 import LampQueryProvider from "@/components/global/QueryProvider"
 
 const BlogListPage = () => {
 	return (<>
-		<GlobalNavbar/>
+		<GlobalNavbar space/>
+		<Jumbotron>
+			<h1 className="text-2xl md:text-4xl font-bold mb-4">Lamp BLOG</h1>
+			<p className="text-md md:text-lg text-slate-400">Explore the latest articles, insights, and stories from my journey!</p>
+		</Jumbotron>
     <LampQueryProvider>
 			<BlogList/>
     </LampQueryProvider>
--- components/global/Navbar.tsx
+++ components/global/Navbar.tsx
@@ -1,16 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/no-html-link-for-pages */
 
-const GlobalNavbar = () => {
+const GlobalNavbar = ({children, space, logo}: {children?: React.ReactNode, space?: boolean, logo?: string}) => {
 	return (
 		<nav className="fixed w-full z-20">
-			<div className="flex justify-between items-center px-12 py-4 bg-gray-800 text-white">
-				<div className="h-8">
-					<img src="/assets/lamp.png" alt="LamP" className="h-full aspect-square" />
+			<div className={
+				`flex justify-between items-center text-white `
+				+ (space ? 'px-8 xl:px-28 py-4 md:py-8' : 'px-6 md:px-12 py-4 bg-gray-800')
+			}>
+				<div className="h-8 md:h-12 flex items-end">
+					<img src={logo || "/assets/lamp.png"} alt="LamP" className="h-full aspect-square" />
 				</div>
 				<ul className="flex space-x-8">
-					<li><a href="/" className="hover:underline">Blog</a></li>
-					<li><a href="/" className="hover:underline">Reach me</a></li>
+					{children ? children : 
+					(<>
+						<li><a href="/" className="hover:underline">Blog</a></li>
+						<li><a href="/" className="hover:underline">Reach me</a></li>
+					</>)}
 				</ul>
 			</div>
 		</nav>
